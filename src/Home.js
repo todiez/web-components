a89465afbd99256d3a7c360c201cdbf4fa1bb5ee
tybo-dev <tobias@diez.guru>
2022-12-17 17:36:51 +0100

    styling of card added

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,44 @@
 const Home = () => {
-    return (  );
-}
- 
+
+    
+  return (
+    <div className="home">
+      <div className="card">
+        <h5 className="card-header">Book Title</h5>
+        <div className="card-body">
+          <h5>subtitle</h5>
+          <div className="card-text">
+            <table className="table">           
+              <tbody>
+                <tr>
+                  <td className="text-success font-weight-bold">Title:</td>
+                  <td>title</td>
+                </tr>
+                <tr>
+                  <td className="text-success font-weight-bold">Subtitle:</td>
+                  <td>subtitle</td>
+                </tr>
+                <tr>
+                  <td className="text-success font-weight-bold">Author:</td>
+                  <td>author</td>
+                </tr>
+                <tr>
+                  <td className="text-success font-weight-bold">Publisher:</td>
+                  <td>publisher</td>
+                </tr>
+                <tr>
+                  <td className="text-success font-weight-bold">
+                    Description:
+                  </td>
+                  <td>description</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Home;
