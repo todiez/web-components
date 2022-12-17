c80a398e5c36154a4ff4cbaf5ce5000a3b33709e
tybo-dev <tobias@diez.guru>
2022-12-17 18:38:29 +0100

    code reduced

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import Home from "./Home";
 
 function App() {
   
-
-
-
   return (
     //JSX inside here
     <div className="App">
@@ -15,55 +12,7 @@ function App() {
       <div className="content">
         <Home />
       </div>
-
     </div>
-
-    // <div className="container">
-    //   <h1>React Components</h1>
-    //   <button onClick={handleAddBook}>Add Book</button>
-
-    //   {loaded &&
-    //     data.map((book) => (
-    //       <div>
-    //         <button onClick={handleDeleteBook} key={book.isbn}>Delete {book.isbn}</button>
-
-    //         <div className="card">
-    //           <h5 className="card-header">{book.title}</h5>
-    //           <div className="card-body">
-    //             <h5>{book.subtitle}</h5>
-    //             <p className="card-text">
-    //               <table className="table">
-    //                 <tr>
-    //                   <td className="text-success font-weight-bold">Title:</td>
-    //                   <td>{book.title}</td>
-    //                 </tr>
-    //                 <tr>
-    //                   <td className="text-success font-weight-bold">Subtitle:</td>
-    //                   <td>{book.subtitle}</td>
-    //                 </tr>
-    //                 <tr>
-    //                   <td className="text-success font-weight-bold">Author:</td>
-    //                   <td>{book.author}</td>
-    //                 </tr>
-    //                 <tr>
-    //                   <td className="text-success font-weight-bold">
-    //                     Publisher:
-    //                   </td>
-    //                   <td>{book.publisher}</td>
-    //                 </tr>
-    //                 <tr>
-    //                   <td className="text-success font-weight-bold">
-    //                     Description:
-    //                   </td>
-    //                   <td>{book.description}</td>
-    //                 </tr>
-    //               </table>
-    //             </p>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     ))}
-    // </div>
   );
 }
 
