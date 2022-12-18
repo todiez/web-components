6397fed1bebc71aba662445633078b30ad09b7e6
tybo-dev <tobias@diez.guru>
2022-12-18 15:19:29 +0100

    custom hook useFetch added

---
+++ src/useFetch.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+    //this is a custom hook, therefore it needs to start with use...
+    //not a component?!?
+
+    const [data, setData] = useState(null);
+    const [loaded, setLoaded] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setTimeout(() => {
+          console.log("use effect ran");
+          fetch(url)
+            .then((resp) => {
+              if (!resp.ok) {
+                throw Error("Could not fetch data");
+              }
+              return resp.json();
+            })
+            .then((dataFetch) => {
+              setData(dataFetch);
+              setLoaded(true);
+              setError(null)
+            })
+            .catch((err) => {
+              setLoaded(true);
+              setError(err.message)        
+            });
+        }, 700);
+      }, [url]);
+
+
+      
+
+      return ({//returning as object of my custom hook
+        data,
+        loaded,
+        error
+      });
+}
+ 
+export default useFetch;
