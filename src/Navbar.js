9baeea2c595fbb61876b3f562617fd7a2f7d2372
tybo-dev <tobias@diez.guru>
2023-02-06 11:49:49 +0100

    file structure changed

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>Book List</h1>
+      <h1>Book-List</h1>
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/addBook">Add New Book</Link>
